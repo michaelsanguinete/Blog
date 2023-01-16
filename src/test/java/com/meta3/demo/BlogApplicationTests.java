a7e50f086b24947a4c03375a153b321aabd28e6a
Michael Sanguinete <michael.sanguinete@gmail.com>
2023-01-16 13:00:49 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/com/meta3/demo/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
